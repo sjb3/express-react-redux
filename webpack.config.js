20d1e0f22930d655e012f79a7435f1f4f9c906c8
Sung J Byun <sungjbyun@D-173-250-234-17.dhcp4.washington.edu>
2017-06-21 20:50:28 -0700

    not a god tut, should i continue?

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 'use strict';
 
-module exports = {
-    entryL './app.js',
-    output: {
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    watch: true,
-    module: {
-        loaders: [
-            {
-                exclude: /(node_modules)/,
-                loaderL 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
+module.exports = {
+  entry: './public/counterApp.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  watch: true,
+  module: {
+    loaders: [
+        {
+            exclude: /(node_modules)/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
             }
-        ]
-    }
+          }
+    ]
+  }
 }
